6cc6cd0e7f9020bff37210935a540a1576de9196
woneychl <woneychl@gmail.com>
2026-09-14 10:35:31 +0900

    feat: BaseEntity, Shift 엔티티 작성

---
+++ backend/src/main/java/com/merge/backend/domain/shift/entity/Shift.java
@@ -0,0 +1,38 @@
+package com.merge.backend.domain.shift.entity;
+
+import com.merge.backend.global.entity.BaseEntity;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import java.time.LocalDateTime;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+
+@Entity
+@Getter
+@NoArgsConstructor
+public class Shift extends BaseEntity {
+
+    @Column(nullable = false)
+    int scheduledId;
+
+    @Column(nullable = false)
+    int memberId;
+
+    LocalDateTime startAt;
+
+    LocalDateTime endAt;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private Status status;
+
+
+    public enum Status {
+        BEFORE_WORK,
+        DURING_WORK,
+        AFTER_WORK
+    };
+}
