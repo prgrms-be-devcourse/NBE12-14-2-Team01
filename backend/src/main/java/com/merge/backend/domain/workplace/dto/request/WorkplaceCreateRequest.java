89a26c2f30d77318502d41997ca3f83d43b3db36
chesu0 <akfl06288@gmail.com>
2026-09-15 17:24:46 +0900

    feat: Workplace 요청값 검증 추가

--- backend/src/main/java/com/merge/backend/domain/workplace/dto/request/WorkplaceCreateRequest.java
+++ backend/src/main/java/com/merge/backend/domain/workplace/dto/request/WorkplaceCreateRequest.java
@@ -1,5 +1,6 @@
 package com.merge.backend.domain.workplace.dto.request;
 
+import jakarta.validation.constraints.NotBlank;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
@@ -7,6 +8,7 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 public class WorkplaceCreateRequest {
 
+    @NotBlank
     private String name;
 
 }
