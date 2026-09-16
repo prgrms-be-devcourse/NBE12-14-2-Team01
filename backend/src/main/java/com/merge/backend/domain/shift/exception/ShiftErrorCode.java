3f7b3c43d021c0b43308e6e79a39ad239c459387
woneychl <woneychl@gmail.com>
2026-09-16 17:54:01 +0900

    feat: shift 관련 에러코드 작성

---
+++ backend/src/main/java/com/merge/backend/domain/shift/exception/ShiftErrorCode.java
@@ -0,0 +1,97 @@
+package com.merge.backend.domain.shift.exception;
+
+
+import com.merge.backend.global.exception.ErrorCode;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+
+@Getter
+@RequiredArgsConstructor
+public enum ShiftErrorCode implements ErrorCode {
+
+    INVALID_INPUT_VALUE(
+        HttpStatus.BAD_REQUEST,
+        "SFT-001",
+        "입력값이 올바르지 않습니다."
+    ),
+
+    INVALID_WORKPLACE_VALUE(
+        HttpStatus.BAD_REQUEST,
+        "SFT-002",
+        "스케줄이 해당 workplace 소속이 아닙니다."
+    ),
+    INVALID_SHIFT_VALUE(
+        HttpStatus.BAD_REQUEST,
+        "SFT-003",
+        "업무가 해당 스케줄 소속이 아닙니다."
+    ),
+
+    INVALID_WORKPLACE_MEMBER_VALUE(
+        HttpStatus.BAD_REQUEST,
+        "SFT-004",
+        "멤버가 해당 workplace 소속이 아닙니다."
+    ),
+
+    INVALID_STATUS_VALUE(
+        HttpStatus.BAD_REQUEST,
+        "SFT-005",
+        "잘못된 상태입니다."
+    ),
+
+    INVALID_TIME_VALUE(
+        HttpStatus.BAD_REQUEST,
+        "SFT-006",
+        "시간 형식이나 값이 올바르지 않습니다."
+    ),
+
+    INVALID_BEFORE_AFTER_VALUE(
+        HttpStatus.BAD_REQUEST,
+        "SFT-007",
+        "근무 시작 시간은 종료 시간보다 이전이어야 합니다."
+    ),
+
+    INVALID_SHIFT_INSCHEDULE(
+        HttpStatus.BAD_REQUEST,
+        "SFT-008",
+        "설정한 근무시간이 스케줄 주간에서 벗어나있습니다."
+    ),
+    DUPLICATE_LOCAL_SCHEDULE_TIME(
+        HttpStatus.BAD_REQUEST,
+        "SFT-009",
+        "동일인물의 근무는 중복될 수 없습니다."
+    ),
+
+    DUPLICATE_GLOBAL_SCHEDULE_TIME(
+        HttpStatus.BAD_REQUEST,
+        "SFT-010",
+        "다른 근무지에서 중복되는 근무가 있습니다."
+    ),
+
+    UNAVAILABLE_TIME_CONFLICT(
+        HttpStatus.BAD_REQUEST,
+        "UNAVAILABLE_TIME_CONFLICT",
+        "해당 구성원이 이 시간에 근무 불가능 일정을 등록했습니다."
+    ),
+
+    NOT_FOUND_ERROR(
+        HttpStatus.NOT_FOUND,
+        "SFT-404",
+        "대상을 찾을 수 없습니다."
+    ),
+
+    NOT_FOUND_SCHEDULE_ERROR(
+        HttpStatus.NOT_FOUND,
+        "SFT-404",
+        "스케줄이 없거나 잘못된 스케줄 ID입니다."
+    ),
+    NOT_FOUND_SHIFT_ERROR(
+        HttpStatus.NOT_FOUND,
+        "SFT-404",
+        "해당 근무를 찾을 수 없습니다."
+    );
+
+    private final HttpStatus httpStatus;
+    private final String code;
+    private final String message;
+}
