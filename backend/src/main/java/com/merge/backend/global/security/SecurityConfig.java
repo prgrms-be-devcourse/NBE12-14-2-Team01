40bb28c8d7a9e03bd8d1bda7712f62382be0a39b
Deokwoo Kim <ejrdn7298@gmail.com>
2026-09-16 16:53:50 +0900

    fix: STATELESS 설정 추가

--- backend/src/main/java/com/merge/backend/global/security/SecurityConfig.java
+++ backend/src/main/java/com/merge/backend/global/security/SecurityConfig.java
@@ -8,6 +8,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import tools.jackson.databind.ObjectMapper;
@@ -29,6 +30,9 @@ public class SecurityConfig {
                 .anyRequest().authenticated()
             )
             .csrf(csrf -> csrf.disable())
+            .sessionManagement(session ->
+                session.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+            )
             .addFilterBefore(customAuthenticationFilter,
                 UsernamePasswordAuthenticationFilter.class)
 
