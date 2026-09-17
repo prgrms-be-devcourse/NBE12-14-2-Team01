560ff292bc8807ca5b7e78e87e58aa7fce80faca
woneychl <woneychl@gmail.com>
2026-09-17 17:02:12 +0900

    fix: RequestMapping("/api/v1") 삭제

--- backend/src/main/java/com/merge/backend/domain/shift/controller/ShiftController.java
+++ backend/src/main/java/com/merge/backend/domain/shift/controller/ShiftController.java
@@ -17,12 +17,10 @@ import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/api/v1")
 public class ShiftController {
 
     private final ShiftService shiftService;
