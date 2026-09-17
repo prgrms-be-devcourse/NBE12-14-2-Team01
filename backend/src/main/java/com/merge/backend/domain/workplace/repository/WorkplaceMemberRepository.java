2d20360ff01ea4e826bc0d691e750f73202a8ff9
woneychl <woneychl@gmail.com>
2026-09-17 14:36:42 +0900

    refactor: 컨벤션 오류 수정

--- backend/src/main/java/com/merge/backend/domain/workplace/repository/WorkplaceMemberRepository.java
+++ backend/src/main/java/com/merge/backend/domain/workplace/repository/WorkplaceMemberRepository.java
@@ -5,6 +5,5 @@ import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface WorkplaceMemberRepository extends JpaRepository<WorkplaceMember, Long> {
-
     Optional<WorkplaceMember> findByWorkplaceIdAndUserId(Long workplaceId, Long actorId);
 }
