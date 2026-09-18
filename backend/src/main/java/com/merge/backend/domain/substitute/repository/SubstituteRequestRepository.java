97c233acfb64584675737a9e327bbcc3ddaa6878
Deokwoo Kim <ejrdn7298@gmail.com>
2026-09-18 12:27:42 +0900

    feat: 활성 대타 요청 존재 여부 조회 Repository 추가

--- backend/src/main/java/com/merge/backend/domain/substitute/repository/SubstituteRequestRepository.java
+++ backend/src/main/java/com/merge/backend/domain/substitute/repository/SubstituteRequestRepository.java
@@ -1,12 +1,11 @@
-package com.merge.backend.domain.substitute;
+package com.merge.backend.domain.substitute.repository;
 
 import com.merge.backend.domain.substitute.entity.RequestStatus;
 import com.merge.backend.domain.substitute.entity.SubstituteRequest;
 import java.util.Collection;
-import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface SubstituteRequestRepository extends JpaRepository<SubstituteRequest, Long>{
 
-    boolean existsByShift_idAndStatusIn(Long shiftId, Collection<RequestStatus> statuses);
+    boolean existsByShift_IdAndStatusIn(Long shiftId, Collection<RequestStatus> statuses);
 }
