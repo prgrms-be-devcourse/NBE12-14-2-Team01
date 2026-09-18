15b4818c69fe02e3960ae2c445188a36baa2310e
woneychl <woneychl@gmail.com>
2026-09-18 16:09:58 +0900

    fix: 중복되는 에러 코드를 수정

--- backend/src/main/java/com/merge/backend/domain/shift/exception/ShiftErrorCode.java
+++ backend/src/main/java/com/merge/backend/domain/shift/exception/ShiftErrorCode.java
@@ -80,39 +80,39 @@ public enum ShiftErrorCode implements ErrorCode {
     ),
     IS_CANCELED_SHIFT(
         HttpStatus.NOT_FOUND,
-        "SFT-012",
+        "SFT-013",
         "해당 근무는 취소되었습니다."
     ),
     FORBIDDEN_ACCESS(
         HttpStatus.FORBIDDEN,
-        "SFT-403",
+        "SFT-014",
         "잘못된 권한입니다."
     ),
     UNAVAILABLE_TIME_CONFLICT(
             HttpStatus.BAD_REQUEST,
-            "UNAVAILABLE_TIME_CONFLICT",
+            "SFT-015",
             "해당 구성원이 이 시간에 근무 불가능 일정을 등록했습니다."
     ),
 
     NOT_FOUND_ERROR(
             HttpStatus.NOT_FOUND,
-            "SFT-404",
+            "SFT-016",
             "대상을 찾을 수 없습니다."
     ),
 
     NOT_FOUND_SCHEDULE_ERROR(
             HttpStatus.NOT_FOUND,
-            "SFT-404",
+            "SFT-017",
             "스케줄이 없거나 잘못된 스케줄 ID입니다."
     ),
     NOT_FOUND_SHIFT_ERROR(
         HttpStatus.NOT_FOUND,
-        "SFT-404",
+        "SFT-018",
         "해당 근무를 찾을 수 없습니다."
     ),
     NOT_FOUND_WORKPLACE_ERROR(
         HttpStatus.NOT_FOUND,
-        "SFT-404",
+        "SFT-014",
         "해당 근무지를 찾을 수 없습니다."
     );
 
