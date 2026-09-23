d767904c41919fc76067f64ef04f07286983285b
Deokwoo Kim <ejrdn7298@gmail.com>
2026-09-23 11:13:28 +0900

    feat: 내가 보낸 요청 조회 Repository 메서드 추가

--- backend/src/main/java/com/merge/backend/domain/substitute/repository/SubstituteRequestRepository.java
+++ backend/src/main/java/com/merge/backend/domain/substitute/repository/SubstituteRequestRepository.java
@@ -3,9 +3,13 @@ package com.merge.backend.domain.substitute.repository;
 import com.merge.backend.domain.substitute.entity.RequestStatus;
 import com.merge.backend.domain.substitute.entity.SubstituteRequest;
 import java.util.Collection;
+import java.util.List;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface SubstituteRequestRepository extends JpaRepository<SubstituteRequest, Long>{
 
     boolean existsByShift_IdAndStatusIn(Long shiftId, Collection<RequestStatus> statuses);
+    @EntityGraph(attributePaths = {"shift.schedule.workplace"})
+    List<SubstituteRequest>findAllByRequesterMember_User_IdOrderByCreateDateDesc(Long userId);
 }
